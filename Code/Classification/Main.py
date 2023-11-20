e2340dfab9bf6ba36b58f0ae94ca65605b40a9d9
bmariswamy5 <bmariswamy5@gwu.edu>
2023-11-19 21:17:22 -0500

    Sentiment analysis

--- Code/Classification/Main.py
+++ Code/Classification/Main.py
@@ -1,20 +1,50 @@
 import streamlit as st
 import utils
-import pandas as pd
 
+
+from gensim.parsing.preprocessing import preprocess_string
 
 def main():
     st.header("Understand the Topic of Different Legal Text Clauses")
     st.divider()
+
     st.subheader("Step 1: Choose a sample text to analyze: \n Sample txt")
     df_file = utils.upload_file("Upload text file")
 
+    if df_file is not None:
+        # Remove URLs from the text
+        cleaned_text = utils.remove_urls(df_file)
+
+        # Display the processed text
+        words = cleaned_text.split()[:300]
+        st.write("Sample Case Text:")
+        st.write(' '.join(words))
+    st.subheader("Step 2: Choose a model from the left sidebar")
+    model_name = utils.sidebar()
+
+    if model_name:
+        st.write(f"Selected model: {model_name}")
+
+    # Perform classification based on the selected model
+    if model_name == "Sentiment Analysis":
+        sentiment_score = utils.analyze_sentiment(df_file)
+
+        st.write(f"Sentiment Score: {sentiment_score}")
 
 
-    if df_file is None:
-        st.stop()
 
-    st.divider()
 
+    elif model_name == "LDA Model":
+
+        topic_words = utils.perform_lda(df_file)
+
+        st.subheader("LDA Model Result:")
+
+        for topic_id, top_words in topic_words:
+            st.write(f"Topic {topic_id + 1}: {', '.join(top_words)}")
+
+        # Display bar chart for topic distribution
+
+        st.bar_chart({f"Topic {topic_id + 1}": len(top_words) for topic_id, top_words in topic_words})
 if __name__ == "__main__":
     main()
--- Code/Classification/utils.py
+++ Code/Classification/utils.py
@@ -3,12 +3,23 @@ import streamlit as st
 import pandas as pd
 
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from gensim import corpora
+from gensim.models import LdaModel
+from gensim.parsing.preprocessing import preprocess_string
 import nltk
 import seaborn as sns
 import matplotlib.pyplot as plt
 from io import StringIO
+def remove_urls(text):
+    # Define a regular expression pattern for matching URLs
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
 
-def upload_file(file_name, num_words=300):
+    # Use the sub method to replace all matches with an empty string
+    cleaned_text = re.sub(url_pattern, '', text)
+
+    return cleaned_text
+def upload_file(file_name):
     # File uploader (for Streamlit)
     file = st.file_uploader(f"Choose the {file_name}", type=["txt"])
 
@@ -16,13 +27,51 @@ def upload_file(file_name, num_words=300):
         # Read the content of the file as bytes
         content_bytes = file.read()
 
-        # Decode the bytes into a string
-        content = content_bytes.decode('utf-8')
+        if content_bytes:
+            # Decode the bytes into a string
+            content = content_bytes.decode('utf-8')
 
-        # Split the content into words
-        words = content.split()
+            return content
+        else:
+            st.error("File is empty. Please choose a file with content.")
+            return None
+    else:
+        return None
+def sidebar():
+    with st.sidebar:
+        genre = st.radio(
+            "Choose your model",
+            ["Sentiment Analysis","LDA Model"],
+            captions=["Sentiment Analysis", "LDA"],
+            index=None,
+        )
+        return genre
 
-        # Display the first 300 words
-        st.write(' '.join(words[:num_words]))
+
+def analyze_sentiment(text):
+    analyzer = SentimentIntensityAnalyzer()
+    sentiment_score = analyzer.polarity_scores(text)['compound']
 
 
+
+    # Classify sentiment as positive, negative, or neutral
+    sentiment_label = "Positive" if sentiment_score >= 0.05 else "Negative" if sentiment_score <= -0.05 else "Neutral"
+    st.write(f"Sentiment: {sentiment_label}")
+
+    return sentiment_score
+
+def perform_lda(text):
+    processed_text = preprocess_string(text)
+    dictionary = corpora.Dictionary([processed_text])
+    corpus = [dictionary.doc2bow(processed_text)]
+
+    # Train the LDA model
+    lda_model = LdaModel(corpus, num_topics=2, id2word=dictionary)
+
+    # Get the words for each topic
+    topic_words = []
+    for topic_id in range(lda_model.num_topics):
+        words = [word for word, _ in lda_model.show_topic(topic_id)]
+        topic_words.append((topic_id, words))
+
+    return topic_words
