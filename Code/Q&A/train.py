bb6de0338ffa31134b015cdcf8f3a8a02028e564
ChiragLakhanpal <39208607+ChiragLakhanpal@users.noreply.github.com>
2023-11-20 23:42:13 -0500

    Adding stater code

--- Code/Q&A/train.py
+++ Code/Q&A/train.py
@@ -1,1 +1,10 @@
+import torch
+import trasformer
 
+
+from datasets import load_dataset
+raw_datasets = load_dataset("glue", "mrpc")
+
+model = AutoModelForSequenceClassification.from_pretrained("path/to/locally/downloaded/model/files")
+
+raw_datasets = load_dataset("path/to/locally/downloaded/dataset/files")
