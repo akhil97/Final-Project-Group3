a3b99b5cdb76ab7562a581fc78ed3901b2a9d252
ChiragLakhanpal <39208607+ChiragLakhanpal@users.noreply.github.com>
2023-11-25 23:22:14 -0500

    Update train.py

--- Code/Q&A/train.py
+++ Code/Q&A/train.py
@@ -16,61 +16,6 @@ max_length = 384
 stride = 128
 
 
-def preprocess_training_examples(examples):
-    questions = [q.strip() for q in examples["question"]]
-    inputs = tokenizer(
-        questions,
-        examples["context"],
-        max_length=max_length,
-        truncation="only_second",
-        stride=stride,
-        return_overflowing_tokens=True,
-        return_offsets_mapping=True,
-        padding="max_length",
-    )
-
-    offset_mapping = inputs.pop("offset_mapping")
-    sample_map = inputs.pop("overflow_to_sample_mapping")
-    answers = examples["answers"]
-    start_positions = []
-    end_positions = []
-
-    for i, offset in enumerate(offset_mapping):
-        sample_idx = sample_map[i]
-        answer = answers[sample_idx]
-        start_char = answer["answer_start"][0]
-        end_char = answer["answer_start"][0] + len(answer["text"][0])
-        sequence_ids = inputs.sequence_ids(i)
-
-        # Find the start and end of the context
-        idx = 0
-        while sequence_ids[idx] != 1:
-            idx += 1
-        context_start = idx
-        while sequence_ids[idx] == 1:
-            idx += 1
-        context_end = idx - 1
-
-        # If the answer is not fully inside the context, label is (0, 0)
-        if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
-            start_positions.append(0)
-            end_positions.append(0)
-        else:
-            # Otherwise it's the start and end token positions
-            idx = context_start
-            while idx <= context_end and offset[idx][0] <= start_char:
-                idx += 1
-            start_positions.append(idx - 1)
-
-            idx = context_end
-            while idx >= context_start and offset[idx][1] >= end_char:
-                idx -= 1
-            end_positions.append(idx + 1)
-
-    inputs["start_positions"] = start_positions
-    inputs["end_positions"] = end_positions
-    return inputs
-
 import pandas as pd
 train_dataset=datasets['consumer_contracts_qa']['train']
 validation_dataset=datasets['consumer_contracts_qa']['test']
@@ -88,7 +33,43 @@ validation_dataset = validation_dataset.map(
     remove_columns=validation_dataset.column_names,
 )
 
+def preprocess_validation_examples(examples):
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=max_length,
+        truncation="only_second",
+        stride=stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
 
+    sample_map = inputs.pop("overflow_to_sample_mapping")
+    example_ids = []
+
+    for i in range(len(inputs["input_ids"])):
+        sample_idx = sample_map[i]
+        example_ids.append(examples["id"][sample_idx])
+
+        sequence_ids = inputs.sequence_ids(i)
+        offset = inputs["offset_mapping"][i]
+        inputs["offset_mapping"][i] = [
+            o if sequence_ids[k] == 1 else None for k, o in enumerate(offset)
+        ]
+
+    inputs["example_id"] = example_ids
+    return inputs
+
+validation_dataset = val_dataset2.map(
+    preprocess_validation_examples,
+    batched=True,
+    remove_columns=val_dataset2.column_names,
+)
+len(validation_dataset)
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 df_train=df_train[['question','human_ans_indices','review','human_ans_spans']]
 df_test=df_test[['question','human_ans_indices','review','human_ans_spans']]
 
@@ -142,43 +123,7 @@ train_dataset = train_dataset2.map(
 len(train_dataset2), len(train_dataset)
 
 
-def preprocess_validation_examples(examples):
-    questions = [q.strip() for q in examples["question"]]
-    inputs = tokenizer(
-        questions,
-        examples["context"],
-        max_length=max_length,
-        truncation="only_second",
-        stride=stride,
-        return_overflowing_tokens=True,
-        return_offsets_mapping=True,
-        padding="max_length",
-    )
 
-    sample_map = inputs.pop("overflow_to_sample_mapping")
-    example_ids = []
-
-    for i in range(len(inputs["input_ids"])):
-        sample_idx = sample_map[i]
-        example_ids.append(examples["id"][sample_idx])
-
-        sequence_ids = inputs.sequence_ids(i)
-        offset = inputs["offset_mapping"][i]
-        inputs["offset_mapping"][i] = [
-            o if sequence_ids[k] == 1 else None for k, o in enumerate(offset)
-        ]
-
-    inputs["example_id"] = example_ids
-    return inputs
-
-validation_dataset = val_dataset2.map(
-    preprocess_validation_examples,
-    batched=True,
-    remove_columns=val_dataset2.column_names,
-)
-len(validation_dataset)
-
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
 import torch
 from transformers import AutoModelForQuestionAnswering
